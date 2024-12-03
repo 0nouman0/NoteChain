7f39363308e99247457f9208d786084de67cd5c6
Rayan9064 <mohammedrayan9064@gmail.com>
2024-12-03 21:40:04 +0530

    initial commit

--- NoteChain/src/App.tsx
+++ NoteChain/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Hero from './components/home/Hero';
+import Features from './components/home/Features';
+import Services from './components/home/Services';
+import Team from './components/home/Team';
+import Testimonials from './components/home/Testimonials';
+import Contact from './components/home/Contact';
+import Footer from './components/layout/Footer';
+import DoctorDashboard from './components/dashboards/DoctorDashboard';
+import PatientDashboard from './components/dashboards/PatientDashboard';
+import { BuildType, OktoProvider } from 'okto-sdk-react';
 
+const OKTO_CLIENT_API_KEY = "8a001bd6-0804-4284-805c-b8191c07bae9";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <OktoProvider apiKey={OKTO_CLIENT_API_KEY} buildType={BuildType.SANDBOX}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="min-h-screen">
+              <Header />
+              <main>
+                <Hero />
+                <Features />
+                <Services />
+                <Team />
+                <Testimonials />
+                <Contact />
+              </main>
+              <Footer />
+            </div>
+          }
+        />
+        <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+        <Route path="/patient-dashboard" element={<PatientDashboard />} />
+      </Routes>
+      </OktoProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
